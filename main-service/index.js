06ba4522789169a9a492a9dc845128bf9673c7ba
marcomorandin <emmeemmenove@gmail.com>
2024-05-01 19:06:38 +0200

    refactor project structure

--- main-service/index.js
+++ main-service/index.js
@@ -1,32 +1,14 @@
-/*const express = require('express')
-const app = express()
-const port = 3000
+const app = require('./app/app.js');
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.locals.db = mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then ( () => {
+    
+    console.log("Connected to Database");
+    
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    });
+    
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})*/
-
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-const User = require("./schemas/User");
-const PvData = require("./schemas/PvData");
-const PvSystem = require("./schemas/PvSystem");
-const WeatherData = require("./schemas/WeatherData");
-const WeatherStation = require("./schemas/WeatherStation");
-
-const main = async () => {
-  mongoose.set('strictQuery', true);
-  await mongoose.connect(`mongodb+srv://${process.env.MONGO_UNAME}:${process.env.MONGO_PASS}@${process.env.MONGO_URL}`);
-
-  //let a = await PvData.create({time: new Date("2015-07-31T02:00:00"), power: 12000, metadata: { pv_id: 20, description: "TRENTO_Sede Nucleo Elicotteri", installed_power: 8572, ws_id: 0, location: { lat: 46.026944, long: 11.126944, alt: 193 }}});
-  //console.log(a);
-
-  let system = await User.find({});
-  console.log(system[0]);
-}
-main();
