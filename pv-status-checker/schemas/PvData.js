03890959520b2005624e65194e020a0d79cf9401
marcomorandin <emmeemmenove@gmail.com>
2024-05-23 22:15:18 +0200

    added status checker

---
+++ pv-status-checker/schemas/PvData.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const PvData = new Schema(
+    {
+        _id: Schema.ObjectId,
+        time: Date,
+        power: Number,
+        predicted_power: Number,
+        metadata: {
+            pv_id: Schema.ObjectId,
+            description: String,
+            installed_power: Number,
+            price: Number,
+            location: {alt: Number, lat: Number, long: Number},
+            ws_id: Schema.ObjectId,
+        }
+    },
+    {
+        timeseries: {
+            timeField: 'time',
+            metaField: 'metadata'
+        },
+    }
+);
+
+module.exports = model("pv_data", PvData);
---
+++ pv-status-checker/schemas/PvSystem.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require("mongoose");
+
+const PvSystem = new Schema({
+    _id: Schema.ObjectId,
+    description: String,
+    installed_power: Number,
+    location: {alt: Number, lat: Number, long: Number,},
+    url: String,
+    ws_id: Schema.ObjectId,
+    status: String
+});
+
+module.exports = model("pv_info", PvSystem);
